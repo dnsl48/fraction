b94c3736965d8c8b598f33069ba11763165fe236
Scott Wilson <scott@propersquid.com>
2022-01-29 18:09:32 -0800

    Add benchmarking

--- benches/bench_fraction.rs
+++ benches/bench_fraction.rs
@@ -1,7 +1,8 @@
 extern crate criterion;
 extern crate fraction;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use fraction::GenericDecimal;
+use fraction::{GenericDecimal, GenericFraction};
+use std::str::FromStr;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decimal u128/u16 init", |b| {
@@ -14,6 +15,27 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| {
             GenericDecimal::<i64, u16>::from(black_box(15978.649));
             GenericDecimal::<i64, u16>::from(black_box(-15978.649));
+        })
+    });
+
+    c.bench_function("Convert int like from str", |b| {
+        b.iter(|| {
+            GenericFraction::<u8>::from_str(black_box("1"));
+            GenericFraction::<u8>::from_str(black_box("100"));
+        })
+    });
+
+    c.bench_function("Convert float like from str", |b| {
+        b.iter(|| {
+            GenericFraction::<u8>::from_str(black_box("1.0"));
+            GenericFraction::<u8>::from_str(black_box("100.001"));
+        })
+    });
+
+    c.bench_function("Convert fraction like from str", |b| {
+        b.iter(|| {
+            GenericFraction::<u8>::from_str(black_box("1/1"));
+            GenericFraction::<u8>::from_str(black_box("255/255"));
         })
     });
 }
